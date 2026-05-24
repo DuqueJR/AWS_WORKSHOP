7237ffb26762dde1aa70358ba81065ef69b6fa5f
DuqueJR <duqueruizjeronimo@gmail.com>
2026-05-24 12:08:21 -0500

    feat(): Desarrollo y despliegue de una aplicación en FastAPI - codigo que fue desplegado en la instancia de EC2

---
+++ PYTHON_OP/FASTAPI_DESPLIEGUE/config.py
@@ -0,0 +1,11 @@
+import os
+
+AWS_BUCKET = "fastapi-user-images-bucket"
+
+DB_CONFIG = {
+    "host": "fastapi-rds-db.c98cscak09ed.us-east-2.rds.amazonaws.com",
+    "database": "fastapi_db",
+    "user": "postgres",
+    "password": "Hola1234",
+    "port": 5432
+}
